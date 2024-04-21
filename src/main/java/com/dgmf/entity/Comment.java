295dd13c10b2c553d963787a03ffba4f9dc8a2ba
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-04-21 07:35:21 +0200

    Creation of the Comment Entity

---
+++ src/main/java/com/dgmf/entity/Comment.java
@@ -0,0 +1,27 @@
+package com.dgmf.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tbl_comment")
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class Comment {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String text;
+    // Hibernate will automatically take the
+    // current Timestamp of the JVM
+    @CreationTimestamp
+    private LocalDateTime creationDate;
+    @UpdateTimestamp
+    private LocalDateTime lastUpdate;
+    private boolean isActive = true;
+}
