6fb31340d6d169d196d80f330fe83a8a59b1e551
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-04-21 07:15:58 +0200

    Creation of the Product Entity

---
+++ src/main/java/com/dgmf/entity/Product.java
@@ -0,0 +1,26 @@
+package com.dgmf.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.Calendar;
+
+@Entity
+@Table(name = "tbl_product")
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class Product {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Calendar customerSince;
+    // Hibernate will automatically take the
+    // current Timestamp of the JVM
+    @CreationTimestamp
+    private LocalDateTime creationDate;
+    @UpdateTimestamp
+    private LocalDateTime lastUpdate;
+    private boolean isActive = true;
+}
